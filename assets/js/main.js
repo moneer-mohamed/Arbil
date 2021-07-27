fb2949105bdddba99aa10eaf9fbdfa33bd5e5212
moneer mohamed <moneermohamed900@gmail.com>
2021-07-27 21:23:09 +0200

    preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,29 @@
 
 $(document).ready(function() {
 
+    function handlePreloader() {
+        if ($('.loader-wrap').length) {
+            $('.loader-wrap').delay(1000).fadeOut(500);
+        }
+        TweenMax.to($(".loader-wrap .overlay_pre"), 1.2, {
+            force3D: true,
+            left: "100%",
+            ease: Expo.easeInOut,
+        });
+    }
+
+    if ($(".preloader-close").length) {
+        $(".preloader-close").on("click", function() {
+            $('.loader-wrap').delay(200).fadeOut(500);
+        })
+    }
+
+    $(window).on('load', function() {
+        handlePreloader();
+    });
+
 
     /* ===============================  click on navbar toggler  =============================== */
-
     $('.icon_bar').on('click', function() {
         $(this).toggleClass('open');
         $('.sidebar').toggleClass("opened");
